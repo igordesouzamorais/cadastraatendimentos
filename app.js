38e056cfd8a050619cfb31a3bbac6cd4d8e2169c
igordesouzamorais <igordesouzamorais@gmail.com>
2016-11-20 12:07:01 -0300

    ajuste caminho banco de dados

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var uristring =
   process.env.MONGODB_URI || 
   'mongodb://localhost/atendimento';
 
-db.connect(uristring, function (err, res) {
+Mongoose.connect(uristring, function (err, res) {
   if (err) { 
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
   } else {
